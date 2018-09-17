0f2f882e8387c2eda29b506ee3ca4b6f2c37d3bd
Edgar K <edos.edgar@gmail.com>
2018-09-17 08:52:54 +0300

    Update main.cpp

--- QuadraticEquation/main.cpp
+++ QuadraticEquation/main.cpp
@@ -7,6 +7,7 @@ int main() {
   std::cout << "Enter the a, b, c coefficients of quadratic equation a*x^2 + b*x + c = 0" << std::endl;
   std::cin >> a >> b >> c;
   size_t  n_roots;
+  /* Any reason except religion not to make n_roots as a return value? */
   SolveSquare(a, b, c, &x1, &x2, &n_roots);
   switch (n_roots) {
     case 0: std::cout << "The quadratic equation has no roots" << std::endl;
@@ -18,6 +19,11 @@ int main() {
     case 2: std::cout << "The quadratic equation has two roots:\n" << "x1 = " << x1 << " x2 = " << x2 << std::endl;
       break;
 
+    /* It not logical here, try to think of me being a newbie in C, it might make me puzzled for a while, 
+       you took n_roots = 3 for the case with infinitely many roots, huh? */
+    /* Suggest you elaborating that point, like think about creating some sort of enum or any constatns standing
+       for such cases. like EQ_INFINITE_ROOTS (and let's say it is equal to 3, but nobody will get puzzled, coz
+       it is somehow hidden, get it? */
     case 3: std::cout << "The quadratic equation has infinitely many roots" << std::endl;
       break;
 
